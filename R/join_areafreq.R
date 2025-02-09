d5e814e75df1d39ebbe6e3b768f4d0f1f12db2c0
lgplgp <lgpaixao@gmail.com>
2025-02-09 21:43:32 +0000

    join_areafreq

---
+++ R/join_areafreq.R
@@ -0,0 +1,21 @@
+#' Join Area and Frequency
+#'
+#' Updates study area's Simple feature collection by adding area and frequency fields, and populating according to the moments of analysis.
+#' Area field is updated for all features.
+#'
+#' @param nmoments a list of sf Simple feature collections representing the moments for analysis
+#' @param sta an sf Simple features collection representing the study area for analysis
+#'
+#' @return an sf Simple features collection representing the study area for analysis, updated with area and frequency values of the different moments of analysis.
+#'
+join_areafreq = function(nmoments, sta){
+
+  for(i in 1:length(nmoments)){
+    statsSum = stats_by_AreaFID(nmoments[[i]])
+    sta = join_FID_AREA(sta, statsSum, paste0(AREA_FIELD_BASE, i), paste0(FREQ_FIELD_BASE, i))
+    cat("Join fields", i, "Done")
+  }
+
+  return(sta)
+
+}
