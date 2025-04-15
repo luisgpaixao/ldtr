889200633737429d7318be6e02980b09ebdf24f6
lgplgp <lgpaixao@gmail.com>
2025-04-15 19:25:47 +0100

    test

--- tests/testthat/test-createLDT.R
+++ tests/testthat/test-createLDT.R
@@ -15,7 +15,7 @@ test_that("error moments less than 2", {
 
 test_that("error not matching numbers", {
 
-  testthat::expect_error(createLDT(T, 2, 'sta.shp', c('m1.shp', 'm2.shp', 'm3.shp'), 10, 10, T, T, 'out.shp'))
-  testthat::expect_error(createLDT(T, 3, 'sta.shp', c('m1.shp', 'm2.shp'), 10, 10, T, T, 'out.shp'))
+  expect_error(createLDT(T, 2, 'sta.shp', c('m1.shp', 'm2.shp', 'm3.shp'), 10, 10, T, T, 'out.shp'))
+  expect_error(createLDT(T, 3, 'sta.shp', c('m1.shp', 'm2.shp'), 10, 10, T, T, 'out.shp'))
 
 })
