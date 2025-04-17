1d1e59596268efccde0e86cddcbb3b8a78c74e20
lgplgp <lgpaixao@gmail.com>
2025-04-18 00:15:57 +0100

    test valid areas

--- R/valid_moment.R
+++ R/valid_moment.R
@@ -10,12 +10,12 @@ valid_moment <- function(mm_path, index){
   
   mm = st_read(mm_path)
   
-  if((is.na(st_crs(mm)$units)) || (is.null(st_crs(mm)$units))){
-    stop(paste0("CRS of moment", index, "not defined, please check your data.\n"), call. = T)
+  if(is.na(st_crs(mm))){
+    stop(paste0("CRS of moment", index, " not defined, please check your data.\n"), call. = T)
   }
 
   if(! all(st_layers(mm_path)$geomtype %in% c("Polygon", "3D Polygon"))){
-    stop(paste0("Moment", index, "geometry type is not Polygon, please check your data.\n"), call. = T)
+    stop(paste0("Moment", index, " geometry type is not Polygon, please check your data.\n"), call. = T)
   }
   
   return(mm)
--- tests/testthat/test-createmoments.R
+++ tests/testthat/test-createmoments.R
@@ -24,3 +24,20 @@ test_that("create moments different crs", {
   expect_message(create_moments(c(m1, m2), stda))
 
 })
+
+test_that("check all empty moments", {
+
+  stda <- system.file("extdata", "mun_portel_etrs.shp", package = "ldtr")
+  m1 <- system.file("extdata", "emptysta.shp", package = "ldtr")
+  m2 <- system.file("extdata", "emptysta.shp", package = "ldtr")
+  outp_dir <- system.file("extdata", package = "ldtr")
+  outp_shp <- file.path(outp_dir, 'outExample.shp')
+
+  obj <- createLDT(T, 2, stda, c(m1, m2), 3000, 3000, F, F, outp_shp)
+  outp <- writeLDT(obj)
+
+  expect_type(st_read(outp_shp), "list")
+
+  file.remove(list.files(outp_dir, pattern = 'outExample', full.names = T, recursive = F))
+
+})
